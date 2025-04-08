2ceb9ff6d9bea9867addaeaca3933827b6d8728e
Runika24 <runikakoosa@gmail.com>
2025-04-08 14:20:33 +0530

    stringmethod

---
+++ index.js
@@ -0,0 +1,27 @@
+str="runika koosa"
+console.log(str.length)
+str1 = str.toLowerCase()
+console.log(str.toLowerCase())
+console.log(str.toUpperCase())
+
+if(str.length>6){
+    console.log(str)
+}
+else{
+    console.log("not found")
+}
+
+str2 = "John";
+console.log(str2.charAt(1));
+let sentence = "I love coding";
+console.log(sentence.indexOf("love"));
+console.log(str.includes("runika")); 
+
+console.log(str.slice(7)); 
+let str0 = "Hello world";
+console.log(str0.replace("world", "there")); 
+ a =str.charAt(4)
+console.log(a)
+
+
+
